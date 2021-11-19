731a17c311dcb36bb4f1ac6b29ba2636b5018f96
emil <emil.h.kangas@gmail.com>
2021-11-19 14:57:36 +0200

    last fix_upload_date_:_:fri 14:57

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -14,7 +14,7 @@ public class ItemCollector : MonoBehaviour
 
             Destroy(collision.gameObject);
             cherries++;
-            cherriesText.text = ("Cherries: " + cherries);
+            cherriesText.text = ("Cherries:" +cherries);
 
         }
     }
