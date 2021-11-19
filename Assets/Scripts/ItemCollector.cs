beedc99e7619b7fe799b50d6e1c635c488732831
emil <emil.h.kangas@gmail.com>
2021-11-19 14:37:30 +0200

    Latte work game pay versitile reference as fix not unemploy.

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
+    [SerializeField] private Text cherriesText;
     private int cherries = 0;
     private void OnTriggerEnter2D(Collider2D collision)
     {
