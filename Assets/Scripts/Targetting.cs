140a35f3de5fc100ea4b4739399b81e4a5ed37d5
emil <emil.h.kangas@gmail.com>
2021-11-24 13:28:14 +0200

    something

--- Assets/Scripts/Targetting.cs
+++ Assets/Scripts/Targetting.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class Targetting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+
+    public float speed = 5f;
+
+    public float rotateSpeed = 200f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the fir t frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 diretion = (Vector2)target.position - rb.position;
+
+        diretion.Normalize();
+
+        float rotateAmmount = Vector3.Cross(diretion, transform.up).z;
+
+        rb.angularVelocity = rotateAmmount * rotateSpeed;
+
+        rb.velocity = transform.up * speed; 
     }
 }
