e038c9977feaee5e87f0694a28ffa1ac937b00d0
emil <emil.h.kangas@gmail.com>
2021-11-25 15:08:05 +0200

    patches

--- Assets/Scripts/FinishSound.cs
+++ Assets/Scripts/FinishSound.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishSound : MonoBehaviour
 {
@@ -19,6 +20,6 @@ public class FinishSound : MonoBehaviour
 
     private void Completelevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 }
